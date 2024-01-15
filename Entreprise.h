091e6b805e5466db8d35e50acbbfb5c792b85f99
Lucas BARABAS <lbarabas@numerique-educatif.fr>
2024-01-15 11:55:19 +0100

    ...

---
+++ Entreprise.h
@@ -0,0 +1,25 @@
+#pragma once
+/*######################Class Entreprise#######################*/
+
+class Entreprise
+{
+};
+
+/*######################Class Employes#######################*/
+
+class Employes
+{
+};
+
+/*######################Class Commerciaux#######################*/
+
+class Commerciaux
+{
+};
+
+/*######################Class Techniciens#######################*/
+
+class Techniciens
+{
+};
+
